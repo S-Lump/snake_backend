98929d5ff2c237fd4e072d4b10801d4719bde655
StupidLamer <grisga2001@gmail.com>
2020-09-17 20:48:39 +0800

    update urls

--- bilogger/bilogger/urls.py
+++ bilogger/bilogger/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from news.views import index
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('news/', include('news.urls')),
