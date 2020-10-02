83bf62274ec0ac6c75e516147d4cb9c589440c5f
StupidLamer <grisga2001@gmail.com>
2020-10-02 14:10:37 +0800

    add tags

--- bilogger/news/templatetags/news_tags.py
+++ bilogger/news/templatetags/news_tags.py
@@ -10,7 +10,7 @@ def get_categories():
     return Category.objects.all()
 
 
-# @register.inclusion_tag('news/list_categories.html')
-# def show_categories():
-#     categories = Category.objects.all()
-#     return {"categories": categories}
+@register.inclusion_tag('news/list_categories.html')
+def show_categories():
+    categories = Category.objects.all()
+    return {"categories": categories}
