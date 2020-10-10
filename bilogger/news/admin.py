bfa0d4cd18e5cbd75a283c6424c27c3d39b0f003
StupidLamer <grisga2001@gmail.com>
2020-10-11 01:56:39 +0800

    change admin panel

--- bilogger/news/admin.py
+++ bilogger/news/admin.py
@@ -1,15 +1,25 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-from .models import News, Category
+from .models import Category, News
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'is_published', 'category')
+    list_display = ('id', 'title', 'created_at', 'is_published', 'category', 'get_photo')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('category', 'is_published')
+    fields = ('title', 'category', 'content', 'is_published', 'photo', 'get_photo')
+    readonly_fields = ('created_at', 'updated_at', 'get_photo')
+    safe_on_top = True
 
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="75">')
+        return '-'
+
+    get_photo.short_description = 'Миниатюра'
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
