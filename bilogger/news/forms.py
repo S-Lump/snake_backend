4aa9b5e21f4795d5a1c2a847cf22db5ae09fa8dc
StupidLamer <grisga2001@gmail.com>
2020-10-13 00:52:11 +0800

    add form of sign up

--- bilogger/news/forms.py
+++ bilogger/news/forms.py
@@ -1,8 +1,33 @@
-from django import forms
-from .models import News
-# from .models import Category
 import re
+from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from .models import News
+
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(
+        label='Имя пользователя', 
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    password1 = forms.CharField(
+        label='Введите пароль', 
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    password2 = forms.CharField(
+        label='Подтвердите пароль', 
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    email = forms.EmailField(
+        label='Почта', 
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
 
 class NewsForm(forms.ModelForm):
